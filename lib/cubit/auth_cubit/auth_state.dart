63c349c4ac9fc0c11d2b9560e2800082491aa5a6
Khammunh Thao <100525451+Khammunh@users.noreply.github.com>
2024-01-04 17:08:55 +0700

    otp class

---
+++ lib/cubit/auth_cubit/auth_state.dart
@@ -0,0 +1,20 @@
+abstract class AuthState {}
+
+class AuthInitialState extends AuthState {}
+
+class AuthLoadingState extends AuthState {}
+
+class AuthCodeSentState extends AuthState {}
+
+class AuthCodeVerifiedState extends AuthState {}
+
+class AuthLoggedInState extends AuthState {}
+
+class AuthLoggedOutState extends AuthState {}
+
+class AuthErrorState extends AuthState {
+  final String error;
+
+  AuthErrorState({required this.error});
+  
+}
