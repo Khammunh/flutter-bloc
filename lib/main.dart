00481951d60366b01c89d6e7947d1d047b459bfe
Khammunh Thao <100525451+Khammunh@users.noreply.github.com>
2024-01-04 15:00:09 +0700

    otp

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutterbloc/cubit/internet_cubit.dart';
 import 'package:flutterbloc/welcome/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MyApp(),
   );
